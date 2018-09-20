df096102da1ef3ffc6a98a9e775f9a55372fbec1
suzinee <ggaebong.u@gmail.com>
2018-09-20 13:39:33 +0900

    ver2 BT - PWM 출력제어 테스트

--- source/proj2/ctrl_uart_motor/main.c
+++ source/proj2/ctrl_uart_motor/main.c
@@ -12,27 +12,35 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include "usart.h"
 #include "UART1.h"
+#include "PWM.h"
 
 static FILE usart1_in = FDEV_SETUP_STREAM(NULL, USART1_receive, _FDEV_SETUP_RW);
 static FILE usart1_out = FDEV_SETUP_STREAM(USART1_send,NULL, _FDEV_SETUP_WRITE);
+volatile uint8_t p_count;
 
 void PIN_Init(void);//PORTA0123 사용
-
 enum{Go = 1, Right, Left, Stop};
+	
+ISR(TIMER0_COMP_vect)
+{
+	p_count++;
+	TCNT0 = 0;
+}
 	
 int main(void)
 {
     //초기화
 	USART1_init(BR9600);
 	PIN_Init();
+	PWM_Init();
 	
 	//입출력을 위한 변수
 	stdin = &usart1_in;
 	stdout = &usart1_out;
 
 	int state_num;
+	//int dim = 0; //바퀴 속도 조절
 	
 	sei();
 	
@@ -54,19 +62,29 @@ int main(void)
 		printf("State: %d \r\n", state_num);
 			
 		PORTA = 0x00;
+		//OCR0 = 128; //비교일치 기준값
+		//TIMSK |= (1<<OCIE0);
+		
+	
 		switch(state_num)
 		{
 			case Go:
 				PORTA |= (1 << PORTA0);	//Green
+				OCR0 = 128;
+				_delay_ms(10);
+				//dim += direction;
 				break;
 			case Right:
 				PORTA |= (1 << PORTA1);	//Yellow
+				OCR0 = 1;
+				_delay_ms(10);
 				break;
 			case Left:
 				PORTA |= (1 << PORTA2);	//Yellow
 				break;
 			case Stop:
 				PORTA |= (1 << PORTA3);	//Red
+				OCR0 = 255;
 				break;
 			default:
 				printf("Please check the number of state.\r\n");
